9765acef7652c1f5e1256bd0933a5788edd44b15
foko53 <jenfoko@gmail.com>
2020-07-16 12:14:34 -0400

    first commit

---
+++ classes/traveler.js
@@ -0,0 +1,18 @@
+class traveler {
+    constructor(name, food, isHealthy) {
+        this.name = name
+        this.food = 1
+        this.isHealthy = true
+    }
+    hunt() {
+        return this.food = + 2
+
+    }
+    eat() {
+        for (let index = 1; index < this.food; index--) {
+            return this.isHealthy = false;
+        }
+    }
+}
+const newTraveler = new traveler('Marie', 'diane', 'Annick')
+
---
+++ classes/wagon.js
@@ -0,0 +1,25 @@
+class wagon {
+    constructor(capacity, passengers) {
+        this.capacity = 2
+        this.passenger = []
+    }
+    getAvailableSeatCount() {
+        for (let index = 0; index < this.capacity.length; index++) {
+            for (let i = 0; i < seats[i].length; i++)
+
+                return false;
+        }
+    }
+
+
+    join() {
+
+
+    }
+    shouldQuarantine() {
+
+    }
+    totalFood() {
+
+    }
+}
